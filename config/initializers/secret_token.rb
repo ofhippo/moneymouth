f18c2be66e8ee59d268f44d4cb418dadd6bffd3d
Dan Rufener <dan@deckmonitoring.com>
2013-05-05 11:29:40 -0700

    Generate rails project
     (and adjust gitignore to the rails default one)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moneymouth::Application.config.secret_token = '922164cc3b9940a141e4728b1ecded8d67e64b23a4e26a118ac8222d47405d505d6953f990c11b9844ecd942fcc07aee6ea6ac4ea1d4825f5cfb71ae0f09ba05'
